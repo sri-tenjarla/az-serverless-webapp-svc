c6cf08d9094ff3acc5bc807cd06f675028f961db
sri-tenjarla <stenjarla@icloud.com>
2024-10-23 21:12:10 +0100

    .Net core 8 MVC project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzAppServiceApp102.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
